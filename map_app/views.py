063b1111f64c920f0062ac5c20d7480e41436657
R2D2 <r2d2@MacBook-Pro-R2D2.local>
2023-08-24 17:01:42 +0600

    marker

--- map_app/views.py
+++ map_app/views.py
@@ -1,11 +1,18 @@
+import json
 from django.shortcuts import render
 from .models import Marker
-from django.core.serializers import serialize
 
 
 def map_view(request):
-    markers = serialize('json', Marker.objects.all())
+    markers = Marker.objects.all()
+    markers_json = json.dumps([{
+        'name': marker.name,
+        'address': marker.address,
+        'latitude': marker.latitude,
+        'longitude': marker.longitude
+    } for marker in markers])
     context = {
-        'markers_json': markers,
+        'markers_json': markers_json,
     }
     return render(request, 'map_app/map.html', context)
+
